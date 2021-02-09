c6f7d2982dd387feddf794caabbb878a2de2fe17
Dylan Barker <dthomasbarker@gmail.com>
2021-02-09 11:48:51 +0000

    Removing redundant references

--- extract.fish
+++ extract.fish
@@ -1,43 +1,44 @@
-function extract
+function x
     for file in $argv
         if test -f $file
             switch $file
                 case '*.cbt' '*.tar.bz2' '*.tar.gz' '*.tar.xz' '*.tbz2' '*.tgz' '*.txz' '*.tar'
                     tar xvf $file
                 case '*.lzma'
-                    unlzma ./$file
+                    unlzma $file
                 case '*.bz2'
-                    bunzip2 ./$file
+                    bunzip2 $file
                 case '*.cbr' '*.rar'
-                    unrar x -ad ./$file
+                    unrar x -ad $file
                 case '*.gz'
-                    gunzip ./$file
+                    gunzip $file
                 case '*.cbz' '*.epub' '*.zip'
-                    unzip ./$file
+                    unzip $file
                 case '*.z'
-                    uncompress ./$file
+                    uncompress $file
                 case '*.7z' '*.apk' '*.arj' '*.cab' '*.cb7' '*.chm' '*.deb' '*.dmg' '*.iso' '*.lzh' '*.msi' '*.pkg' '*.rpm' '*.udf' '*.wim' '*.xar'
-                    7z x ./$file
+                    7z x $file
                 case '*.xz'
-                    unxz ./$file
+                    unxz $file
                 case '*.exe'
-                    cabextract ./$file
+                    cabextract $file
                 case '*.cpio'
-                    cpio -id < ./$file
+                    cpio -id < $file
                 case '*.cba' '*.ace'
-                    unace x ./$file
+                    unace x $file
                 case '*.zpaq'
-                    zpaq x ./$file
+                    zpaq x $file
                 case '*.arc'
-                    arc e ./$file
+                    arc e $file
                 case '*.cso'
-                    ciso 0 ./$file ./'$file.iso' && extract $file.iso && \rm -f $file
+                    ciso 0 $file $file.iso && extract $file.iso && rm -f $file
                 case '*'
                     echo "extract: '$file' - unknown archive method"
+                    return 1
             end
         else
             echo "'$file' - file does not exist"
+            return 1
         end
     end
 end
-
